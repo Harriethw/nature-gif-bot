03b6c77b92e6e14c12806c96c27002033cb8fab8
HarrietHW <harriethw@gmail.com>
2024-07-12 13:26:10 +0200

    add saucy chat

--- NatureGifBot.ts
+++ NatureGifBot.ts
@@ -2,6 +2,7 @@ import { Bot, InlineKeyboard } from "grammy";
 import * as dotenv from 'dotenv'
 import { randomIntFromInterval } from "./utils";
 import { locations } from "./locations";
+import { texts } from "./texts";
 dotenv.config()
 
 //Create a new bot
@@ -20,7 +21,7 @@ bot.on("message", async (ctx) => {
 
   await ctx.replyWithLocation(locations[gifIndex][0],locations[gifIndex][1])
   await ctx.replyWithAnimation(`http://github.com/Harriethw/nature-gif-bot/blob/main/gifs/gif_${gifIndex}.gif?raw=true`)
-  await ctx.reply("Reply to this message for more nature GIFs in your area.")
+  await ctx.reply(texts[gifIndex])
 });
 
 //Start the Bot
---
+++ texts.ts
@@ -0,0 +1,10 @@
+export const texts = [
+    "let's go outside together baby x", 
+    "fresh air makes me feel good x", 
+    "let's go look at plants together sweetie x",
+    "come and see me x",
+    "spend time with me in nature x",
+    "come and find me by the Loire x",
+    "take a stroll and look for me x",
+    "i love to be looked at x"
+]
